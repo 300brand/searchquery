989efd87637e2348dd32cc14e64ab2ef6efd6de3
jake <bigjake2s@gmail.com>
2014-02-14 17:19:39 -0500

    Adding tests with test generator

---
+++ test_generator.pl
@@ -0,0 +1,136 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use Switch;
+use Search::QueryParser;
+use Data::Dumper;
+
+my $header = <<HEADER;
+package searchquery
+
+var tests = []struct {
+	Input  string
+	Query  Query
+	String string
+}{
+HEADER
+my $fmt = <<FMT;
+	{
+		Input:  %s,
+		String: %s,
+		Query: %s	},
+FMT
+my $footer = <<FOOTER;
+}
+FOOTER
+
+my @tests = (
+	{ query => 'a b' },
+	{ query => 'a OR b' },
+	{ query => 'a AND b' },
+	{ query => "txt~'^foo.*' date>='01.01.2001' date<='02.02.2002'" },
+	# Following two should be equivalent:
+	{ query => "a AND (b OR c) AND NOT d" },
+	{ query => "+a +(b c) -d" },
+	{ query => "Id#123,444,555,666 AND (b OR c)" },
+	{ query => '+mandatoryWord -excludedWord +field:word "exact phrase"' },
+	{ query => '"Red Hat" AND Google' },
+	{ query => 'Google AND NOT "Red Hat"' },
+	{ query => '"Red Hat" OR "Fusion IO"' },
+	{ query => '("Cloud Computing" AND "Red Hat") ("Cloud Computing" AND "Fusion IO")' },
+	{ query => '"Cloud Computing" AND ("Red Hat" OR "Fusion IO")' },
+	{ query => '"Colon:In the Tech" AND "Red Hat"' },
+	# Should error: cannot mix AND/OR in requests; use parentheses
+	# { query => '"Cloud Computing" AND "Red Hat" OR "Cloud Computing" AND "Fusion IO"' },
+);
+
+$Data::Dumper::Useqq = 1;
+$Data::Dumper::Varname = '';
+
+print($header);
+foreach (@tests) {
+	my $s = "$_->{query}";
+	my $qp = new Search::QueryParser;
+	my $query;
+	$query = $qp->parse($s) or $query = "Error in query : " . $qp->err;
+	printf($fmt, sdump($s), sdump($qp->unparse($query)), go_Query($query, "\t\t"));
+}
+print($footer);
+
+sub sdump {
+	return substr(Dumper(shift), 5, -2)
+}
+
+sub go_Query {
+	my $q = shift;
+	my $indent = shift;
+	my %keymap = (
+		Required => '+',
+		Optional => '',
+		Excluded => '-',
+	);
+
+	my @subQ;
+	push @subQ, "Query{\n";
+
+	while ((my $name, my $prefix) = each %keymap) {
+		next if not $q->{$prefix};
+
+		push @subQ, "$indent\t$name: []SubQuery{\n";
+		push @subQ, go_SubQuery($_, "$indent\t\t") foreach @{$q->{$prefix}};
+		push @subQ, "$indent\t},\n";
+	}
+	push @subQ, "$indent},\n";
+	return join "", @subQ;
+}
+
+sub go_SubQuery {
+	my $subQ = shift;
+	my $indent = shift;
+	my $str = "${indent}SubQuery{\n";
+
+	# Convert quote to a constant
+	my $quote;
+	switch ($subQ->{quote} || "") {
+		case '"' { $quote = "QuoteDouble"; }
+		case "'" { $quote = "QuoteSingle"; }
+		else     { $quote = "QuoteNone";   }
+	}
+	$str .= "$indent\tQuote:    $quote,\n";
+
+	# Convert operator to a constant
+	my $op;
+	switch ($subQ->{op}) {
+		case "#"  { $op = "OperatorCSV";          }
+		case ":"  { $op = "OperatorField";        }
+		case "~"  { $op = "OperatorRegex";        }
+		case "!~" { $op = "OperatorRegexNeg";     }
+		case "==" { $op = "OperatorRelE";         }
+		case ">"  { $op = "OperatorRelGT";        }
+		case ">=" { $op = "OperatorRelGTE";       }
+		case "<"  { $op = "OperatorRelLT";        }
+		case "<=" { $op = "OperatorRelLTE";       }
+		case "!=" { $op = "OperatorRelNE";        }
+		case "()" { $op = "OperatorSubquery";     }
+		else      { print "wtf is $subQ->{op}\n"; }
+	}
+	$str .= "$indent\tOperator: $op,\n";
+
+	# Add field name if applicable
+	if ($subQ->{field}) {
+		$str .= "$indent\tField:    " . sdump($subQ->{field}) . ",\n";
+	}
+
+	if ($subQ->{op} eq '()') {
+		$str .= "$indent\tQuery: " . go_Query($subQ->{value}, "$indent\t");
+	} else {
+		$str .= "$indent\tPhrase:   " . sdump($subQ->{value}) . ",\n";
+	}
+
+	# return  "(" . go_Query($subQ->{value}) . ")"  if $subQ->{op} eq '()';
+	# my $quote = $subQ->{quote} || "";
+	# return "$subQ->{field}$subQ->{op}$quote$subQ->{value}$quote";
+
+	$str .= "$indent},\n";
+}
---
+++ tests_test.go
@@ -0,0 +1,378 @@
+package searchquery
+
+var tests = []struct {
+	Input  string
+	Query  Query
+	String string
+}{
+	{
+		Input:  "a b",
+		String: ":a :b",
+		Query: Query{
+			Optional: []SubQuery{
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorField,
+					Phrase:   "a",
+				},
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorField,
+					Phrase:   "b",
+				},
+			},
+		},
+	},
+	{
+		Input:  "a OR b",
+		String: ":a :b",
+		Query: Query{
+			Optional: []SubQuery{
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorField,
+					Phrase:   "a",
+				},
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorField,
+					Phrase:   "b",
+				},
+			},
+		},
+	},
+	{
+		Input:  "a AND b",
+		String: "+:a +:b",
+		Query: Query{
+			Required: []SubQuery{
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorField,
+					Phrase:   "a",
+				},
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorField,
+					Phrase:   "b",
+				},
+			},
+		},
+	},
+	{
+		Input:  "txt~'^foo.*' date>='01.01.2001' date<='02.02.2002'",
+		String: "txt~'^foo.*' date>='01.01.2001' date<='02.02.2002'",
+		Query: Query{
+			Optional: []SubQuery{
+				SubQuery{
+					Quote:    QuoteSingle,
+					Operator: OperatorRegex,
+					Field:    "txt",
+					Phrase:   "^foo.*",
+				},
+				SubQuery{
+					Quote:    QuoteSingle,
+					Operator: OperatorRelGTE,
+					Field:    "date",
+					Phrase:   "01.01.2001",
+				},
+				SubQuery{
+					Quote:    QuoteSingle,
+					Operator: OperatorRelLTE,
+					Field:    "date",
+					Phrase:   "02.02.2002",
+				},
+			},
+		},
+	},
+	{
+		Input:  "a AND (b OR c) AND NOT d",
+		String: "+:a +(:b :c) -:d",
+		Query: Query{
+			Excluded: []SubQuery{
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorField,
+					Phrase:   "d",
+				},
+			},
+			Required: []SubQuery{
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorField,
+					Phrase:   "a",
+				},
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorSubquery,
+					Query: Query{
+						Optional: []SubQuery{
+							SubQuery{
+								Quote:    QuoteNone,
+								Operator: OperatorField,
+								Phrase:   "b",
+							},
+							SubQuery{
+								Quote:    QuoteNone,
+								Operator: OperatorField,
+								Phrase:   "c",
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+	{
+		Input:  "+a +(b c) -d",
+		String: "+:a +(:b :c) -:d",
+		Query: Query{
+			Required: []SubQuery{
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorField,
+					Phrase:   "a",
+				},
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorSubquery,
+					Query: Query{
+						Optional: []SubQuery{
+							SubQuery{
+								Quote:    QuoteNone,
+								Operator: OperatorField,
+								Phrase:   "b",
+							},
+							SubQuery{
+								Quote:    QuoteNone,
+								Operator: OperatorField,
+								Phrase:   "c",
+							},
+						},
+					},
+				},
+			},
+			Excluded: []SubQuery{
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorField,
+					Phrase:   "d",
+				},
+			},
+		},
+	},
+	{
+		Input:  "Id#123,444,555,666 AND (b OR c)",
+		String: "+Id#123,444,555,666 +(:b :c)",
+		Query: Query{
+			Required: []SubQuery{
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorCSV,
+					Field:    "Id",
+					Phrase:   "123,444,555,666",
+				},
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorSubquery,
+					Query: Query{
+						Optional: []SubQuery{
+							SubQuery{
+								Quote:    QuoteNone,
+								Operator: OperatorField,
+								Phrase:   "b",
+							},
+							SubQuery{
+								Quote:    QuoteNone,
+								Operator: OperatorField,
+								Phrase:   "c",
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+	{
+		Input:  "+mandatoryWord -excludedWord +field:word \"exact phrase\"",
+		String: "+:mandatoryWord +field:word :\"exact phrase\" -:excludedWord",
+		Query: Query{
+			Optional: []SubQuery{
+				SubQuery{
+					Quote:    QuoteDouble,
+					Operator: OperatorField,
+					Phrase:   "exact phrase",
+				},
+			},
+			Excluded: []SubQuery{
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorField,
+					Phrase:   "excludedWord",
+				},
+			},
+			Required: []SubQuery{
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorField,
+					Phrase:   "mandatoryWord",
+				},
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorField,
+					Field:    "field",
+					Phrase:   "word",
+				},
+			},
+		},
+	},
+	{
+		Input:  "\"Red Hat\" AND Google",
+		String: "+:\"Red Hat\" +:Google",
+		Query: Query{
+			Required: []SubQuery{
+				SubQuery{
+					Quote:    QuoteDouble,
+					Operator: OperatorField,
+					Phrase:   "Red Hat",
+				},
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorField,
+					Phrase:   "Google",
+				},
+			},
+		},
+	},
+	{
+		Input:  "Google AND NOT \"Red Hat\"",
+		String: "+:Google -:\"Red Hat\"",
+		Query: Query{
+			Required: []SubQuery{
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorField,
+					Phrase:   "Google",
+				},
+			},
+			Excluded: []SubQuery{
+				SubQuery{
+					Quote:    QuoteDouble,
+					Operator: OperatorField,
+					Phrase:   "Red Hat",
+				},
+			},
+		},
+	},
+	{
+		Input:  "\"Red Hat\" OR \"Fusion IO\"",
+		String: ":\"Red Hat\" :\"Fusion IO\"",
+		Query: Query{
+			Optional: []SubQuery{
+				SubQuery{
+					Quote:    QuoteDouble,
+					Operator: OperatorField,
+					Phrase:   "Red Hat",
+				},
+				SubQuery{
+					Quote:    QuoteDouble,
+					Operator: OperatorField,
+					Phrase:   "Fusion IO",
+				},
+			},
+		},
+	},
+	{
+		Input:  "(\"Cloud Computing\" AND \"Red Hat\") (\"Cloud Computing\" AND \"Fusion IO\")",
+		String: "(+:\"Cloud Computing\" +:\"Red Hat\") (+:\"Cloud Computing\" +:\"Fusion IO\")",
+		Query: Query{
+			Optional: []SubQuery{
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorSubquery,
+					Query: Query{
+						Required: []SubQuery{
+							SubQuery{
+								Quote:    QuoteDouble,
+								Operator: OperatorField,
+								Phrase:   "Cloud Computing",
+							},
+							SubQuery{
+								Quote:    QuoteDouble,
+								Operator: OperatorField,
+								Phrase:   "Red Hat",
+							},
+						},
+					},
+				},
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorSubquery,
+					Query: Query{
+						Required: []SubQuery{
+							SubQuery{
+								Quote:    QuoteDouble,
+								Operator: OperatorField,
+								Phrase:   "Cloud Computing",
+							},
+							SubQuery{
+								Quote:    QuoteDouble,
+								Operator: OperatorField,
+								Phrase:   "Fusion IO",
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+	{
+		Input:  "\"Cloud Computing\" AND (\"Red Hat\" OR \"Fusion IO\")",
+		String: "+:\"Cloud Computing\" +(:\"Red Hat\" :\"Fusion IO\")",
+		Query: Query{
+			Required: []SubQuery{
+				SubQuery{
+					Quote:    QuoteDouble,
+					Operator: OperatorField,
+					Phrase:   "Cloud Computing",
+				},
+				SubQuery{
+					Quote:    QuoteNone,
+					Operator: OperatorSubquery,
+					Query: Query{
+						Optional: []SubQuery{
+							SubQuery{
+								Quote:    QuoteDouble,
+								Operator: OperatorField,
+								Phrase:   "Red Hat",
+							},
+							SubQuery{
+								Quote:    QuoteDouble,
+								Operator: OperatorField,
+								Phrase:   "Fusion IO",
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+	{
+		Input:  "\"Colon:In the Tech\" AND \"Red Hat\"",
+		String: "+:\"Colon:In the Tech\" +:\"Red Hat\"",
+		Query: Query{
+			Required: []SubQuery{
+				SubQuery{
+					Quote:    QuoteDouble,
+					Operator: OperatorField,
+					Phrase:   "Colon:In the Tech",
+				},
+				SubQuery{
+					Quote:    QuoteDouble,
+					Operator: OperatorField,
+					Phrase:   "Red Hat",
+				},
+			},
+		},
+	},
+}
